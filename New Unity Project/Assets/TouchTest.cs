53ca430924fbc4837981080d3843a145d02efa2c
Ryan <ryanl@ideum.com>
2015-10-01 13:55:28 -0600

    Initial Commit

---
+++ New Unity Project/Assets/TouchTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchTest : MonoBehaviour {
+
+	public Texture2D touchTexture;
+
+	// Update is called once per frame
+	void OnGUI () {
+		Touch[] touches = Input.touches;
+		Debug.Log ("TOUCHES: " + touches.Length);
+		for(int i = 0; i < touches.Length; i++) {
+			Vector2 pos = new Vector2(touches[i].position.x - 32f, Screen.height - touches[i].position.y - 32f);
+			GUI.DrawTexture(new Rect(pos, Vector2.one * 64f), touchTexture);
+			GUI.Label(new Rect(pos + Vector2.up * 42f, new Vector2(100f, 30f)), "Finger ID: " + touches[i].fingerId);
+			GUI.Label(new Rect(pos + Vector2.up * 82f, new Vector2(100f, 30f)), "X: " + touches[i].position.x + " Y: " + touches[i].position.y);
+		}
+	}
+}
